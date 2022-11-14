f7c7a6fbbb924c9aff1dcd76b9b6a1bbfbe5e2d4
Berta Blanc <bertablanc89@gmail.com>
2022-11-14 18:00:52 +0100

    finish lab

--- labs/lab-javascript-basic-algorithms-master/js/index.js
+++ labs/lab-javascript-basic-algorithms-master/js/index.js
@@ -5,3 +5,13 @@
 
 // Iteration 3: Loops
 console.log("I am ready");
+
+let hacker1 = "Berta"
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Jofre Tharrats"
+console.log(`The driver's name is ${hacker2}`);
+
+console.log(`${hacker1} have ${hacker1.length} caracters`);
+
+console.log(`${hacker2} have ${hacker2.length} caracters`);
