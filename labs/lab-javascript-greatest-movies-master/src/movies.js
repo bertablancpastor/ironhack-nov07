319446898ddf0e3ba007767bb0a9c2ae8a763208
Berta Blanc <bertablanc89@gmail.com>
2022-11-16 12:46:50 +0100

    1a parte 16112022

--- labs/lab-javascript-greatest-movies-master/src/movies.js
+++ labs/lab-javascript-greatest-movies-master/src/movies.js
@@ -1,7 +1,11 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+const movies = require("./data")
+
+function getAllDirectors(moviesArray) {
+
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {}
@@ -23,3 +27,7 @@ function turnHoursToMinutes(moviesArray) {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
+
+console.log(movies);
+
+
